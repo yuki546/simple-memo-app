946efe2d26532091a19abe0ed758daf484c6e573
yuki546 <diesunabe@gmail.com>
2026-04-22 22:45:30 +0900

    GitHub Pagesへデプロイする設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "REPOSITORY_NAME" : "./",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
